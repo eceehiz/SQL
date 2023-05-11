d2b7f90d98bcffc52a491a8b64202e78ded96e57
Ece <120506489+eceehiz@users.noreply.github.com>
2023-05-11 20:16:40 +0300

    Add files via upload

---
+++ 1_komutlar.sql
@@ -0,0 +1,24 @@
+--DML (Data Manipulation Language) datayla ilgili iþlemler:
+
+--SELECT: Veritabanýndan kayýt çekmek için kullanýlýr
+--INSERT: Tabloya veri eklemek için kullanýlýr
+--UPDATE: Tablo kayýt güncellemek için kullanýlýr
+--DELETE: Tablodan kayýt silmek için kullanýlýr
+--MERGE – UPSERT iþlemi (ekleme veya güncelleme), baþka bir ifadeyle birleþtirme yapar
+--CALL – PL/SQL veya Java alt programýný çalýþtýrýr.
+--EXPLAIN PLAN – Verilere eriþim yolunun detaylarýný açýklamak için kullanýlýr
+--LOCK TABLE – Kontrolü eþ zamanlýlýðý saðlamak için kullanýlýr
+
+--------------------------------------------------------------------------------------------------
+
+--DDL (Data Definination Language) tablonun kendisiyle alakalý iþlemler:
+
+--CREATE: Veri tabanýnda nesneler oluþturmak için kullanýlýr
+--ALTER: Veri tabanýnda nesnelerin yapýsýný deðiþtirmek için kullanýlýr
+--DROP: Veri tabanýndali nesneleri silmek için kullanýlýr
+--TRUNCATE: Boþluklar dahil bir tablodaki tüm kayýtlarý kaldýrmak için kullanýlýr
+--COMMENT: Veri sözlüðüne yorum eklemek için kullanýlýr
+--RENAME: Bir nesneyi yeniden adlandýrmak için kullanýlýr
+
+
+
