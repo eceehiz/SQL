592ad2b3e4891768b6cb78f05aa86706ad4105cb
Ece <120506489+eceehiz@users.noreply.github.com>
2023-05-15 17:16:20 +0300

    Add files via upload

---
+++ 8_view.sql
@@ -0,0 +1,75 @@
+-------------VÝEW------------
+--Sanal tablo oluþturma. metot mantýðý
+
+create view urunListesi
+as
+select u.*, k.KategoriAdi from Urunler u
+left join Kategoriler k on u.KategoriID=k.KategoriID
+go
+
+alter view urunListesi
+as
+select u.UrunAdi,u.BirimFiyati, k.KategoriAdi,k.Tanimi, t.MusteriAdi, t.SirketAdi from Urunler u
+left join Kategoriler k on u.KategoriID=k.KategoriID
+left join Tedarikciler t on t.TedarikciID=u.TedarikciID
+go
+
+select * from urunListesi
+--view her çaðrýldýðýnda sorgu çalýþýr. ama oluþturulurken bir kez derlenir
+
+--personelsatýþ viewi oluþtýralým. hangi personel ne kadar satýþ yapmýþ
+go
+create view PersonelSatisRaporu
+as
+select p.Adi, u.UrunAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim)) as ToplamTutar from Satislar s
+left join Personeller p on p.PersonelID=s.PersonelID
+left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
+left join Urunler u on u.UrunID=sd.UrunID
+group by p.Adi, u.UrunAdi
+
+--hangi ürün hangi müþteriye toplam ne kadar satýlmýþtýr, musteriUrunRaporu viewi
+go
+create view musteriUrunRaporu
+as
+select  u.UrunAdi, m.MusteriAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim)) as ToplamTutar from Musteriler m
+left join Satislar s on m.MusteriID=s.MusteriID
+left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
+left join Urunler u on u.UrunID=sd.UrunID
+group by u.UrunAdi, m.MusteriAdi
+
+select * from musteriUrunRaporu
+
+
+go
+alter view musteriUrunRaporu1
+with encryption  --kendin bir yerde sakladýktan sonra koda ulaþýlmasýn istiyorsan ekle
+as
+select  u.UrunAdi, m.MusteriAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim)) as ToplamTutar, count(u.UrunAdi) Adet from Musteriler m
+left join Satislar s on m.MusteriID=s.MusteriID
+left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
+left join Urunler u on u.UrunID=sd.UrunID
+group by u.UrunAdi, m.MusteriAdi
+
+--hangi nakliyeci hangi ürünü hangi müþteriye adet ve toplam olarak ne satmýþtýr. nakliyeciRaporu
+go
+create view nakliyeciRaporu
+as
+select n.SirketAdi, u.UrunAdi, m.MusteriAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim)) as ToplamTutar,count(u.UrunAdi) as Adet from Nakliyeciler n
+left join Satislar s on n.NakliyeciID=s.ShipVia
+left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
+left join Musteriler m on m.MusteriID=s.MusteriID
+left join Urunler u on u.UrunID=sd.UrunID
+group by n.SirketAdi, u.UrunAdi, m.MusteriAdi
+
+select * from nakliyeciRaporu
+
+
+
+
+
+
+
+
+
+
+
