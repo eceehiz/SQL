8bf3d16a2bc5ccc49bb90df9f682565edda3cbbc
Ece <120506489+eceehiz@users.noreply.github.com>
2023-05-15 17:25:16 +0300

    Rename 9-procedure.sql to 09-procedure.sql

--- 09-procedure.sql
+++ 09-procedure.sql
@@ -1,26 +1,26 @@
---Procedure: viewin parametreli hali. C#'daki metotların parametre alanı.
---veriyi direkt almayı engelliyor, view'i kontrollü alıyoruz
+--Procedure: viewin parametreli hali. C#'daki metotlarÄ±n parametre alanÄ±.
+--veriyi direkt almayÄ± engelliyor, view'i kontrollÃ¼ alÄ±yoruz
 create procedure UrunListele
 as
 select * from Urunler
-exec UrunListele --select ile değil böyle çağırınca geliyor
+exec UrunListele --select ile deÄŸil bÃ¶yle Ã§aÄŸÄ±rÄ±nca geliyor
 
---tüm müşterileri listeleyen prosedür yazalım
+--tÃ¼m mÃ¼ÅŸterileri listeleyen prosedÃ¼r yazalÄ±m
 create procedure MusteriListele
 as
 select * from Musteriler
 exec MusteriListele
 
---ürün adı, fiyatı ve stoğu dışarıdan alan, urunler tablosuna ekleyen procedure yazalım
+--Ã¼rÃ¼n adÄ±, fiyatÄ± ve stoÄŸu dÄ±ÅŸarÄ±dan alan, urunler tablosuna ekleyen procedure yazalÄ±m
 go
 alter proc UrunEkle   --procedure yerine proc da yazabilirsin
-@urunAdi nvarchar(50),   --parametreleri @ ile tanımlıyoruz
+@urunAdi nvarchar(50),   --parametreleri @ ile tanÄ±mlÄ±yoruz
 @fiyat money,
 @stok smallint
 as
 if exists (select * from Urunler where UrunAdi=@urunAdi)
-begin                           --begin end birden fazla satır varsa kullanılıyor, araya yazılıyor
-	print 'Bu üründen vardır'
+begin                           --begin end birden fazla satÄ±r varsa kullanÄ±lÄ±yor, araya yazÄ±lÄ±yor
+	print 'Bu Ã¼rÃ¼nden vardÄ±r'
 	update Urunler set HedefStokDuzeyi+=@stok where UrunAdi=@urunAdi 
     select * from Urunler where UrunAdi=@urunAdi
 end
@@ -34,7 +34,7 @@ end
 exec UrunEkle 'Kiraz', 60,200
 select * from Urunler
 
---dışarıdan gönderilen nakliyeciyi kaydeden prosedürü yazın
+--dÄ±ÅŸarÄ±dan gÃ¶nderilen nakliyeciyi kaydeden prosedÃ¼rÃ¼ yazÄ±n
 go
 create proc NakliyeciEkle
 @sirketAdi varchar(50),
@@ -43,10 +43,10 @@ as
 insert Nakliyeciler(SirketAdi,Telefon)
 values (@sirketAdi, @telefon)
 
-exec NakliyeciEkle 'Akpınar Nakliyat', '(555) 44-2211'
+exec NakliyeciEkle 'AkpÄ±nar Nakliyat', '(555) 44-2211'
 select * from Nakliyeciler
 
---nakliyecilerin tüm bilgilerini güncelleyen proc
+--nakliyecilerin tÃ¼m bilgilerini gÃ¼ncelleyen proc
 go
 alter proc NakliyeciGuncelle
 @sirketAdi varchar(50),
@@ -56,7 +56,7 @@ if not exists(select * from Nakliyeciler where SirketAdi=@sirketAdi)
 begin
 	insert into Nakliyeciler(SirketAdi,Telefon)
 	values (@sirketAdi, @telefon)
-	print 'Ürün eklendi'
+	print 'ÃœrÃ¼n eklendi'
 end
 
 else
@@ -64,13 +64,13 @@ begin
     update Nakliyeciler
     set SirketAdi=@sirketAdi, Telefon=@telefon
     where SirketAdi=@sirketAdi
-	print 'Ürün güncellendi'
+	print 'ÃœrÃ¼n gÃ¼ncellendi'
 end
 	
 exec NakliyeciGuncelle 'Aras Kargo', '(555) 44-33-85'
 select * from Nakliyeciler
 
---dışarıdan girilen bilgiye göre nakliyeciyi silen prosedür
+--dÄ±ÅŸarÄ±dan girilen bilgiye gÃ¶re nakliyeciyi silen prosedÃ¼r
 go
 alter proc NakliyeciSil
 @nakliyeciID int
@@ -82,12 +82,12 @@ begin
 	print 'Nakliyeci silindi'
 end
 else
-   print 'Bu Id ye sahip nakliyeci olmadığından silinemedi'
+   print 'Bu Id ye sahip nakliyeci olmadÄ±ÄŸÄ±ndan silinemedi'
 exec NakliyeciSil 5
 
 select * from Nakliyeciler
 
---aynı anda birden fazla nakliyeciyi silmek istersek, 4den başla 8 e kadar mesela
+--aynÄ± anda birden fazla nakliyeciyi silmek istersek, 4den baÅŸla 8 e kadar mesela
 go
 alter proc NakliyeciSil
 @baslangic int,
@@ -104,17 +104,17 @@ exec NakliyeciSil 3,4
 
 select * from Nakliyeciler
 
---trim komutunu kullanalım
-exec UrunEkle '       Kiraz',  60,300   --bunu eklememesi gerekiyordu ama ekledi. kontrol altına almak için trim kullanalım
+--trim komutunu kullanalÄ±m
+exec UrunEkle '       Kiraz',  60,300   --bunu eklememesi gerekiyordu ama ekledi. kontrol altÄ±na almak iÃ§in trim kullanalÄ±m
 
 create proc UrunSil
 @urunAdi varchar(50)
 as
 delete Urunler where UrunAdi=ltrim(rtrim(@urunAdi))
 
---id parametresi alıp o id li urunun stoğunu [ ya da 10] kullanıcıdan gelen oran kadar arttıran procu yazalım
+--id parametresi alÄ±p o id li urunun stoÄŸunu [ ya da 10] kullanÄ±cÄ±dan gelen oran kadar arttÄ±ran procu yazalÄ±m
 
-create proc StokArttır
+create proc StokArttÄ±r
 @urunID int
 as
 if exists (select * from Urunler where UrunID=@urunID)
@@ -124,8 +124,8 @@ set HedefStokDuzeyi+=10
 where UrunID=@urunID
 end
 else
-print 'Bu ürünün ID si ürün listesinde mevcut değil'
-exec StokArttır 15
+print 'Bu Ã¼rÃ¼nÃ¼n ID si Ã¼rÃ¼n listesinde mevcut deÄŸil'
+exec StokArttÄ±r 15
 select * from Urunler
 
 
