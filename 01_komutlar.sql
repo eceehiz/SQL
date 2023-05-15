5b2e368af032a069c33eed6fcd5e100e3b643694
Ece <120506489+eceehiz@users.noreply.github.com>
2023-05-15 17:20:40 +0300

    Rename 1_komutlar.sql to 01_komutlar.sql

---
+++ 01_komutlar.sql
@@ -0,0 +1,24 @@
+--DML (Data Manipulation Language) datayla ilgili işlemler:
+
+--SELECT: Veritabanından kayıt çekmek için kullanılır
+--INSERT: Tabloya veri eklemek için kullanılır
+--UPDATE: Tablo kayıt güncellemek için kullanılır
+--DELETE: Tablodan kayıt silmek için kullanılır
+--MERGE – UPSERT işlemi (ekleme veya güncelleme), başka bir ifadeyle birleştirme yapar
+--CALL – PL/SQL veya Java alt programını çalıştırır.
+--EXPLAIN PLAN – Verilere erişim yolunun detaylarını açıklamak için kullanılır
+--LOCK TABLE – Kontrolü eş zamanlılığı sağlamak için kullanılır
+
+--------------------------------------------------------------------------------------------------
+
+--DDL (Data Definination Language) tablonun kendisiyle alakalı işlemler:
+
+--CREATE: Veri tabanında nesneler oluşturmak için kullanılır
+--ALTER: Veri tabanında nesnelerin yapısını değiştirmek için kullanılır
+--DROP: Veri tabanındali nesneleri silmek için kullanılır
+--TRUNCATE: Boşluklar dahil bir tablodaki tüm kayıtları kaldırmak için kullanılır
+--COMMENT: Veri sözlüğüne yorum eklemek için kullanılır
+--RENAME: Bir nesneyi yeniden adlandırmak için kullanılır
+
+
+
