0c5640eb0af692e24bc4812d02e794af4a717c2a
Ece <120506489+eceehiz@users.noreply.github.com>
2023-05-15 17:23:01 +0300

    Rename 5_join türleri.sql to 05_join türleri.sql

--- 05_join türleri.sql
+++ 05_join türleri.sql
@@ -1,35 +1,35 @@
 /*
 Outer join 
-3 çeþit outer join vardýr 
-Left Outer Join : Left tablo ilk yazýlan soldaki tablodur. Sol tablodaki tüm kayýtlar gelir null olsa dahi ve sað tablodan sol tablonun iliþkili kayýtlarý getirilir. 
-(sol tablo + sað’ýn solla iliþkili olan kýsmý)*/
+3 Ã§eÅŸit outer join vardÄ±r 
+Left Outer Join : Left tablo ilk yazÄ±lan soldaki tablodur. Sol tablodaki tÃ¼m kayÄ±tlar gelir null olsa dahi ve saÄŸ tablodan sol tablonun iliÅŸkili kayÄ±tlarÄ± getirilir. 
+(sol tablo + saÄŸâ€™Ä±n solla iliÅŸkili olan kÄ±smÄ±)*/
 
 select * from Kategoriler k left outer join Urunler u on  k.KategoriID=u.KategoriID where k.KategoriID is null
 select * from Urunler u left outer join Kategoriler k on u.KategoriID=k.KategoriID where u.UrunID is null
 
-/*Right Outer Join : Right tablo ilk yazýlan tablodur. 
-Sað tablodaki tüm kayýrtlar gelir null olsa bile sol tablodan sað tablonun  iliþkili kayýtlarý getirilir.(sað tablo + sol’un saðla iliþkili olan kýsmý)*/
+/*Right Outer Join : Right tablo ilk yazÄ±lan tablodur. 
+SaÄŸ tablodaki tÃ¼m kayÄ±rtlar gelir null olsa bile sol tablodan saÄŸ tablonun  iliÅŸkili kayÄ±tlarÄ± getirilir.(saÄŸ tablo + solâ€™un saÄŸla iliÅŸkili olan kÄ±smÄ±)*/
 
 select * from Kategoriler k right join Urunler u on  u.KategoriID=k.KategoriID where k.KategoriID is null
 select * from Urunler u right join Kategoriler k on u.KategoriID=k.KategoriID where u.KategoriID is null
 
 
---Full Outer Join: hem saðdaki hem soldaki tablodan null kayýtlar dahil tüm kayýtlarý getirir
+--Full Outer Join: hem saÄŸdaki hem soldaki tablodan null kayÄ±tlar dahil tÃ¼m kayÄ±tlarÄ± getirir
 
 select * from Kategoriler k full join Urunler u on u.KategoriID=k.KategoriID where k.KategoriID is null or u.KategoriID is null
 select * from Urunler k full join Kategoriler u on u.KategoriID=k.KategoriID
 
---hiç satýþ yapýlmayan müþterileri tablo halinde ekrana yaz
+--hiÃ§ satÄ±ÅŸ yapÄ±lmayan mÃ¼ÅŸterileri tablo halinde ekrana yaz
 select * from Musteriler m
 left join Satislar s on s.MusteriID=m.MusteriID
 where s.MusteriID is null
 
---hiç nakliye yapmayan nakliyeciler
+--hiÃ§ nakliye yapmayan nakliyeciler
 select * from Nakliyeciler n left join Satislar s on n.NakliyeciID=s.ShipVia
 where s.ShipVia is null
 
 ----------------------------
---cross join: yazýlan tablolardaki tüm kayýtlarý birbiri ile kartezyen çarparak birbiriyle iliþkilendirir
+--cross join: yazÄ±lan tablolardaki tÃ¼m kayÄ±tlarÄ± birbiri ile kartezyen Ã§arparak birbiriyle iliÅŸkilendirir
 
 select * from Urunler cross join Kategoriler
 
@@ -37,23 +37,23 @@ select p.Adi, u.UrunAdi, t.SirketAdi from Urunler u
 cross join Personeller p
 cross join Tedarikciler t
 
-/*kümeleme fonksiyonlarý: birleþtirme (union), kesiþim(intersect) ve fark(except) olmak üzere 3e ayrýlýr
-farklý 2 sorgu üzerine kümeleme fonksiyonlarý uygulanýrsa; kolon sayýsý ve kolon tipi ayný olmalýdýr
+/*kÃ¼meleme fonksiyonlarÄ±: birleÅŸtirme (union), kesiÅŸim(intersect) ve fark(except) olmak Ã¼zere 3e ayrÄ±lÄ±r
+farklÄ± 2 sorgu Ã¼zerine kÃ¼meleme fonksiyonlarÄ± uygulanÄ±rsa; kolon sayÄ±sÄ± ve kolon tipi aynÄ± olmalÄ±dÄ±r
 */
 select SirketAdi from Tedarikciler
 union 
 select SirketAdi from Musteriler
 
 select SirketAdi from Tedarikciler
-intersect                           --kesiþim
+intersect                           --kesiÅŸim
 select SirketAdi from Musteriler
 
 select SirketAdi from Tedarikciler   --29
-except                              --ortak 2 tane olduðunu düþün
+except                              --ortak 2 tane olduÄŸunu dÃ¼ÅŸÃ¼n
 select SirketAdi from Musteriler    --91,  cevap 27
 
 
---hangi personel hangi üründen satýþ yapmamýþ
+--hangi personel hangi Ã¼rÃ¼nden satÄ±ÅŸ yapmamÄ±ÅŸ
 select p.Adi, u.UrunAdi from Urunler u cross join Personeller p
 except
 select p.Adi, u.UrunAdi from Personeller p
@@ -62,26 +62,26 @@ left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
 left join Urunler u on u.UrunID=sd.UrunID
 
 
---hangi tedarikçi hangi ürünü hiç tedarik etmemiþtir
+--hangi tedarikÃ§i hangi Ã¼rÃ¼nÃ¼ hiÃ§ tedarik etmemiÅŸtir
 select u.UrunAdi, t.SirketAdi  from Tedarikciler t cross join Urunler u
 except
 select u.UrunAdi, t.SirketAdi from Tedarikciler t
 left join Urunler u on t.TedarikciID=u.TedarikciID
 
---hangi ürün hangi müþteriye hiç satýlmamýþtýr
+--hangi Ã¼rÃ¼n hangi mÃ¼ÅŸteriye hiÃ§ satÄ±lmamÄ±ÅŸtÄ±r
 select u.UrunAdi, m.MusteriAdi from Urunler u cross join Musteriler m
 except
-select u.UrunAdi, m.MusteriAdi from Urunler u --left joinle baþladýysan öyle devam etmek mantýklý oluyor
+select u.UrunAdi, m.MusteriAdi from Urunler u --left joinle baÅŸladÄ±ysan Ã¶yle devam etmek mantÄ±klÄ± oluyor
 left join [Satis Detaylari] sd on sd.UrunID=u.UrunID
 left join Satislar s on s.SatisID=sd.SatisID
 left join Musteriler m on m.MusteriID=s.MusteriID
 
---satýþ fiyatý 1000 dolardan fazla olan ürünleri hangi personel satmýþtýr
+--satÄ±ÅŸ fiyatÄ± 1000 dolardan fazla olan Ã¼rÃ¼nleri hangi personel satmÄ±ÅŸtÄ±r
 select u.UrunAdi, p.Adi from Urunler u cross join Personeller p
 except
-select p.Adi, u.UrunAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim)) as Tutar from Personeller p
+select p.Adi, u.UrunAdi, sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ä°ndirim)) as Tutar from Personeller p
 left join Satislar s on s.PersonelID=p.PersonelID
 left join [Satis Detaylari] sd on sd.SatisID=s.SatisID
 left join Urunler u on u.UrunID=sd.UrunID
 group by u.UrunAdi, p.Adi
-having sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ýndirim))>1000
+having sum(sd.BirimFiyati*sd.Miktar*(1-sd.Ä°ndirim))>1000
